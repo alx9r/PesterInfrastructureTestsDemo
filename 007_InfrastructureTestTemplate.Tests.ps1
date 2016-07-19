cb2cb41360c4892c51ef200a25e92b0e0837f446
alx9r <eggy.island9@gmail.com>
2016-07-19 12:54:54 -0700

    add the template

---
+++ 007_InfrastructureTestTemplate.Tests.ps1
@@ -0,0 +1,29 @@
+﻿$computernames = '127.0.0.1','localhost'
+$records = @{}
+
+foreach ( $computername in $computernames )
+{
+    $record = @{}
+    $records.$computername = $record
+    Describe "Computer $computername" {
+        It 'get some object' {
+            # $record.ObjectName = Get-SomeObject
+        }
+    }
+}
+foreach ( $computername in $records.Keys )
+{
+    $record = $records.$computername
+    Describe "Computer $computername" {
+        It 'interrogate the object' {
+            # $record.ObjectName | Should be something
+        }
+    }
+}
+
+
+# To save $records:
+# $records | Export-Clixml "$env:USERPROFILE\desktop\OsVersionRecords.xml"
+
+# To open saved $records:
+# $records = Import-Clixml "$env:USERPROFILE\desktop\OsVersionRecords.xml"
