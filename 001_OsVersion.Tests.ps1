ce9b4cd752d83ac2c8442b72258a5b044512e9a2
alx9r <eggy.island9@gmail.com>
2016-07-19 11:39:31 -0700

    adding OsVersion.Tests.ps1

---
+++ 001_OsVersion.Tests.ps1
@@ -0,0 +1,7 @@
+﻿Describe 'Operating System Version' {
+    It 'is Windows 8.1 or Server 2012R2' {
+        Get-WmiObject Win32_OperatingSystem |
+            % Version |
+            Should be '6.3.9600'
+    }
+}
---
+++ 002_OsVersion.Tests.ps1
@@ -0,0 +1,7 @@
+﻿Describe 'Operating System Version' {
+    It 'is Windows 8.1 or Server 2012R2' {
+        $r = Get-WmiObject Win32_OperatingSystem |
+            % Version
+        $r | Should match '^6\.3\.'
+    }
+}
