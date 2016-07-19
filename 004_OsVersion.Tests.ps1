ffeaa8a4326719436d44daf3925762f0b728387f
alx9r <eggy.island9@gmail.com>
2016-07-19 11:56:37 -0700

    add looping

---
+++ 004_OsVersion.Tests.ps1
@@ -0,0 +1,14 @@
+﻿$computernames = 'localhost','127.0.0.1'
+
+foreach ( $computername in $computernames )
+{
+    Describe "Operating System Version on $computername" {
+        It 'is Windows 8.1 or Server 2012R2' {
+            # Don't overspecify your tests.
+            $r = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername |
+                % Version
+            $r.Split('.')[0] | Should be 6
+            $r.Split('.')[1] | Should be 3
+        }
+    }
+}
