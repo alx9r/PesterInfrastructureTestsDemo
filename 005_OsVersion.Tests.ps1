9bb78329472498312ebf7471bb8aa7b8b9d073a0
alx9r <eggy.island9@gmail.com>
2016-07-19 12:39:10 -0700

    add 005_OsVersion.Tests.ps1

---
+++ 005_OsVersion.Tests.ps1
@@ -0,0 +1,38 @@
+﻿# Record the object for each computer.
+
+$computernames = '127.0.0.2','localhost'
+
+# Create a variable to record the objects you are interrogating.
+$records = @{}
+foreach ( $computername in $computernames )
+{
+    # Create a new "record" for each computername
+    $record = @{}
+    $records.$computername = $record
+
+    Describe "Computer $computername" {
+        # If retrieval fails, this test will fail.
+        # This is a prerequisite to the other test, so it goes first.
+        It 'get Win32_OperatingSystem WMI Object' {
+            $splat = @{
+                ComputerName = $computername
+                Class = 'Win32_OperatingSystem'
+            }
+            # record the object in its raw form
+            $record.Win32_OperatingSystem = Get-WmiObject @splat
+            $record.Win32_OperatingSystem |
+                Should not beNullOrEmpty
+        }
+        It 'extract the VersionString' {
+            $record.OSVersion = $record.Win32_OperatingSystem | % Version
+            $record.OSVersion | Should not beNullOrEmpty
+            $record.OSVersion | Should beOfType 'string'
+        }
+        # If OS Version is wrong, this test will fail.
+        # This depends on retrieving the WMI object, so it goes after
+        # that step.
+        It "version matches pattern" {
+            $record.OSVersion | Should match '6\.1'
+        }
+    }
+}
