ca15658bcf8553ddd9388f3346348cca6bb32e74
alx9r <eggy.island9@gmail.com>
2016-07-19 11:48:49 -0700

    relax overfitting

---
+++ 003_OsVersion.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Describe 'Operating System Version' {
+    It 'is Windows 8.1 or Server 2012R2' {
+        # Don't overspecify your tests.
+        $r = Get-WmiObject Win32_OperatingSystem |
+            % Version
+        $r.Split('.')[0] | Should be 6
+        $r.Split('.')[1] | Should be 3
+    }
+}
