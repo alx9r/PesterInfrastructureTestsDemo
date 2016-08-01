26a117dbdcf8f9d87fea0254a3d93a2626ccf40a
alx9r <eggy.island9@gmail.com>
2016-07-31 20:19:29 -0700

    remove reference to Server 2012 move comments to bottom

--- 003_OsVersion.Tests.ps1
+++ 003_OsVersion.Tests.ps1
@@ -1,5 +1,5 @@
 ﻿Describe 'Operating System Version' {
-    It 'is Windows 8.1 or Server 2012R2' {
+    It 'is Windows 8.1' {
         # Don't overspecify your tests.
         $r = Get-WmiObject Win32_OperatingSystem |
             % Version
--- 004_OsVersion.Tests.ps1
+++ 004_OsVersion.Tests.ps1
@@ -1,14 +1,10 @@
 ﻿# apply the same test to multiple computers
 $computernames = 'localhost','127.0.0.2'
 
-# This has problems:
-#  1. How do you efficiently look at the WMI object from a particular computer?
-#  2. Errors retrieving the WMI object are incorrectly reported as incorrect
-#     OS Versions.
 foreach ( $computername in $computernames )
 {
     Describe "Operating System Version on $computername" {
-        It 'is Windows 8.1 or Server 2012R2' {
+        It 'is Windows 8.1' {
             $r = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername |
                 % Version
             $r.Split('.')[0] | Should be 6
@@ -16,3 +12,8 @@ foreach ( $computername in $computernames )
         }
     }
 }
+
+# This has problems:
+#  1. How do you efficiently look at the WMI object from a particular computer?
+#  2. Errors retrieving the WMI object are incorrectly reported as incorrect
+#     OS Versions.
