d80d604b001284e8d1c4c200049a63802643d43d
alx9r <eggy.island9@gmail.com>
2016-08-12 11:45:15 -0700

    add screen resolution test

--- setup.ps1
+++ setup.ps1
@@ -3,3 +3,11 @@ Set-Location "$env:USERPROFILE\Desktop\PesterInfrastructureTestsDemo"
 explorer .
 ise .\000_Intro.ps1
 Clear-Host
+
+Describe 'screen resolution' {
+    It 'should be 1280x800' {
+        $r = Get-WmiObject Win32_DesktopMonitor
+        $r.ScreenHeight | Should be 800
+        $r.ScreenWidth | Should be 1280
+    }
+}
