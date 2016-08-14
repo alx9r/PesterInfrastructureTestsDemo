e13c80db206e207385578729ec752af4a761b3e1
alx9r <eggy.island9@gmail.com>
2016-08-14 09:09:43 -0700

    split scope tests into three parts

---
+++ 011_scope.Tests.ps1
@@ -0,0 +1,18 @@
+﻿Describe 'variable created in parent scope to "it" block' {
+    # A variable set in a parent scope can be read...
+    # but if you modify it a copy of that variable is
+    # made in the local scope.
+    $describe_var = 'original value'
+    It 'can read variable from parent scope' {
+        $describe_var | Should be 'original value'
+    }
+    It 'can modify that variable locally' {
+        $describe_var = 'new value'
+        $describe_var | Should be 'new value'
+    }
+    It 'modifying that variable locally does not change variable in parent scope' {
+        $describe_var = 'new value'
+        $r = Get-Variable describe_var -Scope 1 -ValueOnly
+        $r | Should be 'original value'
+    }
+}
---
+++ 012_scope.Tests.ps1
@@ -0,0 +1,19 @@
+﻿Describe 'members of objects created in parent scope' {
+    # The members of objects created in parent scope to it block...
+    # can be modified in "it" block.
+    # That way you can access data inside an "it" block
+    # from code outside it.
+    $describe_hashtable = @{
+        Member = 'original value'
+    }
+    It 'can read a member of variable in parent scope' {
+        $describe_hashtable.Member | Should be 'original value'
+    }
+    It 'can modify a member of a variable in parent scope' {
+        $describe_hashtable.Member = 'new value'
+        $describe_hashtable.Member | Should be 'new value'
+    }
+    It 'the modified value of that member is available in another "it" scope' {
+        $describe_hashtable.Member | Should be 'new value'
+    }
+}
